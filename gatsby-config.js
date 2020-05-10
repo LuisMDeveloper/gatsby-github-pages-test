98c14d5992c756d7d93848c844c5cddac95301bd
Ramirez Vargas, Luis Manuel <LRamirezVargas@luxoft.com>
2020-05-10 16:19:05 -0500

    try to fix broken images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/gatsby-github-pages-test",
   siteMetadata: {
     title: `Gatsby Github Pages Test`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -31,5 +32,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-  pathPrefix: "/gatsby-github-pages-test",
 }
